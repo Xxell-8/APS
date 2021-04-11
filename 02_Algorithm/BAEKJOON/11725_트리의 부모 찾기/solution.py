848b8c475b66bfa4786a7d554a02f3ed16e033b1
Xxell-8 <cell8pepper@gmail.com>
2021-04-11 19:50:09 +0900

    BOJ 11725 트리의 부모 찾기

---
+++ 02_Algorithm/BAEKJOON/11725_트리의 부모 찾기/solution.py
@@ -0,0 +1,25 @@
+import sys
+#sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+n = int(input())
+tree = [[] for _ in range(n+1)]
+
+for _ in range(n-1):
+    a, b = map(input().split())
+    tree[a].append(b)
+    tree[b].append(a)
+
+result = [0 for _ in range(n+1)]
+
+stack = [1]
+while stack:
+    node = stack.pop()
+    for edge in tree[node]:
+        if not result[edge]:
+            result[edge] = node
+            stack.append(edge)
+            tree[edge].remove(node)
+
+for i in range(2, n+1):
+    print(result[i])
